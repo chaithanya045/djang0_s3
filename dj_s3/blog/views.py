94798378a9a5ff06007d4ad90a5df071a05cf1eb
chaithanya045 <104122945+chaithanya045@users.noreply.github.com>
2022-05-23 19:32:17 +0530

    Delete dj_s3 directory

--- dj_s3/blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from .models import BlogPost
-
-def home(request):
-    return render(request,'home.html',{'posts':BlogPost.objects.all()})
-
-    
